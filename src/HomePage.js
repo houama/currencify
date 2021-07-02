4fce76d0d806e9f5608397668311dc84a6daee83
houama <captainhoauam@gmail.com>
2021-07-03 02:27:18 +0700

    first commit

---
+++ src/HomePage.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import HomeView from './HomeView';
+
+function HomePage() {
+
+    //--Make State--//
+
+    const [dataCurrency, setDataCurrency] = useState([])
+    const [optionCurrency, setOptionCurrency] = useState([])
+    const [displaySelectedCurrency, setDisplaySelectedCurrency] = useState()
+    const [selectedCurrency, setSelectedCurrency] = useState([])
+    const [amount, setAmount] = useState(1)
+    const [displayButton, setDisplayButton] = useState(false)
+    const [displayAddButton, setDisplayAddButton] = useState(true)
+    const [displayCurrList, setDisplayCurrList] = useState(false)
+
+
+    //---Get API---//
+
+    useEffect(() => {
+    
+        async function getAPI(){
+            await axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=${process.env.REACT_APP_API_KEY}&symbols=USD,CAD,IDR,GBP,CHF,SGD,INR,MYR,JPY,KRW`)
+            .then(res => {
+                const data = res.data;
+                Object.keys(data.rates).map((datas) => {
+                    if(datas != 'USD'){
+                        setOptionCurrency(optionCurrency => [...optionCurrency, datas])
+                    }
+                })
+                
+                    setDataCurrency(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            }
+            getAPI();
+    }, [])
+
+    
+    //---FUNCTION---//
+
+    function handleChange (e) {
+        setDisplaySelectedCurrency(e.target.value)
+        console.log(e.target.value)
+    }
+
+    function handleSubmit () {
+        setSelectedCurrency(selectedCurrency => [...selectedCurrency,displaySelectedCurrency])
+        setDisplayButton(false)
+        setDisplayCurrList(false)
+        setDisplayAddButton(true)
+        
+        setOptionCurrency(optionCurrency.filter(optionDelete =>(optionDelete !== displaySelectedCurrency)))
+    }
+
+    function handleChangeAmount(e) {
+        if(e.target.value >= 0){
+            setAmount(e.target.value)
+        }
+    }
+
+
+    function handleDelete (index){
+        setSelectedCurrency(selectedCurrency.filter(currDelete =>(currDelete !== index)))
+        setOptionCurrency(optionCurrency => [...optionCurrency,index])
+    }
+
+    function handleAdd(){
+        setDisplayAddButton(false)
+        setDisplayCurrList(true)
+        setDisplayButton(true)
+
+        setDisplaySelectedCurrency(optionCurrency[0])
+
+    }
+
+    //---Declare Variable to Accomodate Currency Option---//
+    let curr_list
+
+    //---If all currency option have been selected, user will get "No more options"---//
+    if(optionCurrency.length != 0){
+        curr_list = optionCurrency.map(currencies => (<option value={currencies}>{currencies}</option>))
+    }else{
+        curr_list = (<option>No more options</option>)
+    }
+
+    //---Declare variable and save currency alias---//
+    const curr_alias = ['CAD - Canadian Dollars', 'IDR - Indonesian Rupiah', 'GBP - Pound Sterling', 'CHF - Swiss Franc', 'SGD - Singapore Dollars', 'INR - Indian Rupee', 'MYR - Malaysian Ringgit', 'JPY - Japanese Yen', 'KRW - South Korean Won']
+    
+    //---Return---//
+        return (
+                <HomeView
+                    dataCurr = { dataCurrency }
+                    inputAmount = { amount }
+                    onChangeAmount = { handleChangeAmount }
+                    onSelectedCurrency = { selectedCurrency }
+                    currencyAlias = { curr_alias }
+                    currencyDelete = { handleDelete }
+                    addButton = { displayAddButton }
+                    currList = { displayCurrList }
+                    submitButton = { displayButton }
+                    add = { handleAdd }
+                    dispSelectedCurrency = { displaySelectedCurrency }
+                    changeCurrency = { handleChange }
+                    currencyList = { curr_list }
+                    submit = { handleSubmit }
+                />
+
+        );
+}
+
+export default HomePage;
---
+++ src/HomeView.js
@@ -0,0 +1,159 @@
+import React from 'react'
+import {
+    Card, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
+function HomeView(props){
+    const{
+        dataCurr,
+        inputAmount,
+        onChangeAmount,
+        onSelectedCurrency,
+        currencyAlias,
+        currencyDelete,
+        addButton,
+        currList,
+        submitButton,
+        add,
+        dispSelectedCurrency,
+        changeCurrency,
+        currencyList,
+        submit
+
+    } = props
+
+    //---Declare variable---//
+    var usd_amount, converted_amount, converted_rate
+
+    //---Make function to handle real-time update of converted currency based on exhcange rate from API and input amount in USD
+    function handleUpdateCurrency (index) {
+        usd_amount = inputAmount / dataCurr.rates[`USD`]
+        converted_amount = (usd_amount * dataCurr.rates[`${index}`]).toFixed(2)
+
+        converted_rate = ((1 / dataCurr.rates[`USD`]) * dataCurr.rates[`${index}`]).toFixed(2)
+        
+
+        // console.log(usd_amount)
+        // console.log(converted_amount)
+    }
+
+    //---Return JSX to HomePage---//
+    return(
+        <div>
+            <div className="container-sm my-5">
+                <div className="row">
+                    <div className="col-lg-6 mx-auto">
+                    <Card>
+                    <CardBody>
+                    <CardTitle tag="h3">Currencify</CardTitle>
+                    <CardSubtitle tag="h6" className="text-muted">Online Foreign Exchange Currency<br></br>From USD</CardSubtitle>
+                    
+                        <div className="row" style={{marginTop:"50px"}}>
+                                <div className="col-md">
+                                    <text style={{fontSize:"14px",float:"left"}}><i>USD - United State Dollars</i></text>
+                                </div>
+                        </div>
+                        <div className="row">
+                            <div className="col"> 
+                                    <h5 style={{float:"left"}}>USD</h5>
+                            </div>
+                            <div className="col">
+                                {/* Input amount of USD */}
+                                <input type="number" className="input" value={inputAmount} min="1"  onChange={onChangeAmount} />
+                            </div>
+
+                        </div>
+                        
+                    <div className="row" style={{marginTop:"50px"}}>
+                        <div className="col">
+
+                            { onSelectedCurrency.map( (selected) => {
+
+
+                                  handleUpdateCurrency(selected)
+
+                                return (
+                                    currencyAlias.map((alias) => {
+                                        if(alias.includes(selected)){
+      
+                                            return (
+                                                
+                                                <div className="card gedf-card mt-3">
+                                                    <div className="card-header">
+                                                                <div className="mr-2">
+                                                                    <div className="row" style={{marginTop:"50px"}}>
+                                                                        <div className="col">
+                                                                        
+                                                                            <h5 style={{float:"left"}}>{selected}</h5>
+                                                        
+                                                                        </div>
+                                                                    <div className="col">
+                                                                        {/* Output amount of converted amount in form of several currencies */}
+                                                                        <input type="number" className="input" value={converted_amount} disabled/>
+                                                                    </div>
+                                                                        
+                                                                    </div>
+                                                                    <div className="row">
+                                                                        <div className="col">
+                                                                            {/* Alias of currencies, ex: Indonesian Rupiah */}
+                                                                            <text style={{fontSize:"14px",float:"left"}}><i>{alias}</i></text>
+            
+                                                                        </div>
+                                                                    </div>  
+                                                                    <div className="row">
+                                                                        <div className="col">
+                                                                            {/* Exchange rate of selected currency */}
+                                                                            <text style={{fontSize:"14px",float:"left"}}><i>1 USD = {selected} {converted_rate}</i></text>
+
+                                                                        </div>
+                                                                        <div className="col">
+                                                                            {/* Delete converted currency */}
+                                                                            <Button type="submit" className="btn btn-danger" onClick={ () => currencyDelete(selected) } style={{marginTop:"10px"}} >Delete</Button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                    
+                                                    </div>
+                                                </div>
+                                            )
+
+                                        }
+                                    
+                                    }) 
+                                )
+
+                                }) 
+                            }
+                            {/* Add More Currencies Button */}
+                                { addButton ? <Button  className="btn btn-info" onClick={add} style={{marginTop:"30px"}}><b>Add More Currencies</b></Button> : null }
+
+                            {/* Dropdown list of currencies */}
+                                { currList ? 
+                                <div>
+                                    <div className="row" style={{marginTop:"30px"}}>
+                                        <text>Please choose the currency</text>
+                                    </div>
+                                    <div className="row container">
+                                        <select className="custom-select" value={dispSelectedCurrency} onChange={changeCurrency} style={{marginTop:"15px"}}> {currencyList} </select> 
+                                    </div>
+                                </div> : null }    
+                                    
+                            {/* Add Button */}
+                                { submitButton ? <Button type="submit" className="btn btn-success" onClick={submit} style={{marginTop:"10px"}}>Add</Button> : null }
+
+                   
+                            </div>
+                        </div>
+                    </CardBody>
+                        
+                </Card>
+
+            </div>
+            </div>
+            </div>
+            </div>
+        );
+}
+
+export default HomeView;
